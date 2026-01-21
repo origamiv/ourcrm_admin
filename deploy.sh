c69e3900fc2b063cb667d2dee4f3b4bdc11a60f7
Вениамин Смородинский <origamiv@gmail.com>
2026-01-21 14:17:38 +0300

    vristo

--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,5 @@ echo '------------ php'
 /usr/bin/php -v
 echo '------------ composer'
 /usr/bin/php /usr/local/bin/composer install
-echo '------------ migrate'
-/usr/bin/php artisan migrate
-#/usr/bin/php artisan migrate:fresh --seed
-/usr/bin/php artisan db:seed
-#/opt/php83/bin/php artisan migrate
-echo '------------ swagger'
-/usr/bin/php artisan scramble:export --path=public/docs/openapi.json
-# /usr/bin/php artisan scribe:generate
-# /opt/php83/bin/php artisan l5-swagger:generate
+echo '------------ npm'
+npm run build
