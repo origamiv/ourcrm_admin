01d51fb051c68fb777ecb50432389872b4b235a6
Вениамин Смородинский <origamiv@gmail.com>
2026-01-29 07:19:33 +0300

    Страница формы

--- app/Http/Controllers/CrudController.php
+++ app/Http/Controllers/CrudController.php
@@ -7,17 +7,32 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Symfony\Component\HttpFoundation\Request;
+use GuzzleHttp\Client;
 
 class CrudController extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function index(Request $request) {
-        $paths=explode('/',$request->getRequestUri());
-        $entity=$paths[1];
-        return view('apps.invoice.list',['config'=>config('entities.'.$entity)]);
+    public function index(Request $request)
+    {
+        $paths = explode('/', $request->getRequestUri());
+        $entity = $paths[1];
+
+        return view('apps.invoice.list', [
+            'config' => config('entities.' . $entity),
+        ]);
     }
-    public function show($id) {
-        return view('apps.invoice.preview');
+
+    public function show($id, Request $request)
+    {
+        $paths  = explode('/', $request->getRequestUri());
+        $entity = $paths[1];
+
+        $config = config('entities.' . $entity);
+
+        return view('apps.invoice.show', [
+            'config' => $config,
+        ]);
     }
+
 }
