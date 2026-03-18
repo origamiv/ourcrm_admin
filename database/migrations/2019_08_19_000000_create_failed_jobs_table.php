e2ab86a0abe0f8f0afd193713dbdf2e28cab1adf
Вениамин Смородинский <origamiv@gmail.com>
2026-03-18 14:59:42 +0000

    fix

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
+        return;
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
